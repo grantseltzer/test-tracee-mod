d4497689d54d8e356b6e029549dd115affdc8ee0
grantseltzer <grantseltzer@gmail.com>
2021-09-30 14:50:00 -0400

    update to latest version :D
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/grantseltzer/test-tracee-mod
 
 go 1.16
 
-require github.com/grantseltzer/tracee v1.2.4
+require (
+	github.com/grantseltzer/tracee/tracee-ebpf v1.6.9
+	github.com/grantseltzer/tracee/tracee-ebpf/external v1.6.9
+	github.com/grantseltzer/tracee/tracee-rules v1.6.9
+)
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/grantseltzer/tracee/tracee-ebpf/external"
 	"github.com/grantseltzer/tracee/tracee-ebpf/tracee"
+	"github.com/grantseltzer/tracee/tracee-rules/engine"
+	"github.com/grantseltzer/tracee/tracee-rules/types"
 )
 
 func main() {
-
-	fmt.Println(tracee.Config{
-		PerfBufferize: 2,
-	})
+	fmt.Println(external.Event{})
+	fmt.Println(tracee.Tracee{})
+	fmt.Println(types.SignatureMetadata{})
+	fmt.Println(engine.Engine{})
 }
